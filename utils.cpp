396e3591c9750e96034fbee5623a08e9f2832514
tomsmeding <hallo@tomsmeding.nl>
2015-12-29 20:12:34 +0100

    utils addition: read files and split on null

--- utils.cpp
+++ utils.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
 #include <glob.h>
 #include "utils.h"
@@ -38,6 +39,29 @@ vector<string> expand_glob(vector<string> globs) {
 		res.push_back(pglob.gl_pathv[i]);
 	}
 	globfree(&pglob);
+	return res;
+}
+
+
+vector<string> read_line_split_null(istream &f){
+	vector<string> res;
+	string str;
+	do {
+		getline(f,str,'\0');
+		if(str.size()==0)break;
+		res.push_back(move(str));
+	}while(f);
+	return res;
+}
+
+vector<vector<string>> read_file_split_null(const string &fname){
+	ifstream f(fname);
+	vector<vector<string>> res;
+	while(f){
+		vector<string> r=move(read_line_split_null(f));
+		if(r.size()==0)continue;
+		res.push_back(move(r));
+	}
 	return res;
 }
 
--- utils.h
+++ utils.h
@@ -8,4 +8,6 @@ namespace Vergeten {
 vector<string> expand_glob(string path);
 vector<string> expand_glob(vector<string> paths);
 
+vector<vector<string>> read_file_split_null(const string &fname);
+
 }
