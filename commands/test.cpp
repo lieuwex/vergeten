5a65d512108c13c5a97dbb6a7e6317fbccb38cbb
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-12-29 16:53:25 +0100

    update test command

--- commands/test.cpp
+++ commands/test.cpp
@@ -1,7 +1,8 @@
-namespace Vergeten {
+#include "../base.h"
+using namespace Vergeten;
 
 void test_init(void){
-	;
-}
-
+	register_command({"test"}, [] (int argc, char **argv) -> int {
+		cerr << "kaas!" << endl;
+	});
 }
