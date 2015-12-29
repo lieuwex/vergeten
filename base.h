b7e38ee17c8d9399439c0b7948cf9ff6ecedd86e
tomsmeding <hallo@tomsmeding.nl>
2015-12-29 14:38:22 +0100

    base stuff

--- base.h
+++ base.h
@@ -1,6 +1,10 @@
 #include <string>
 
+namespace Vergeten {
+
 typedef int (*command_fn_t)(int argc,char **argv);
 
 void register_command(string name,command_fn_t func);
 void register_command(vector<string> names,command_fn_t func); //with aliases
+
+}; //namespace
